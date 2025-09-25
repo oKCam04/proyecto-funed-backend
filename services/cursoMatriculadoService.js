53815a3a905d0be8e28db100684adf27dac7cf7b
Camilo <ch385593@gmail.com>
2025-09-25 15:06:57 -0500

    consulta cursoActivoporpersona funcional

--- services/cursoMatriculadoService.js
+++ services/cursoMatriculadoService.js
@@ -36,6 +36,55 @@ class MatricularCursoService {
             throw new Error("Error al eliminar matrícula: " + error.message);
         }
     }
+
+    static async cursoMatriculadoPersona(id_persona){
+        try{
+           const matriculas = await cursomatriculado.findAll({
+            where: {
+                id_persona: id_persona,
+                estado: 'Activo'
+            },
+            include: [
+                {
+                model: require('../models').ofertacurso, // tu modelo ofertacurso
+                as: 'curso', // alias definido en la asociación
+                include: [
+                    {
+                    model: require('../models').curso, // tu modelo curso
+                    as: 'curso'
+                    }
+                ]
+                }
+            ]
+            })
+
+             return matriculas.map((m) => ({
+                idMatricula: m.id,
+                estado: m.estado,
+                resultado: m.resultado,
+                idPersona: m.id_persona,
+
+                // Oferta del curso
+                idOferta: m.curso?.id,
+                codigo: m.curso?.codigo_curso,
+                fechaInicio: m.curso?.fecha_inicio_curso,
+                fechaFin: m.curso?.fecha_fin_curso,
+                horario: m.curso?.horario,
+                cupos: m.curso?.cupos,
+                precio: m.curso?.precio,
+                foto: m.curso?.foto,
+
+                // Datos del curso base
+                idCurso: m.curso?.curso?.id,
+                nombre: m.curso?.curso?.nombre_curso,
+                duracion: m.curso?.curso?.duracion,
+                temario: m.curso?.curso?.temario,
+                tipo: m.curso?.curso?.tipo_curso,
+                }));
+        }catch(error){
+            throw new Error("Error al crear matrícula: " + error.message);
+        }
+    }
 }
 
 module.exports = MatricularCursoService;
