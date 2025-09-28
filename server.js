4cdb47b41016c1ceab4a65ee809a53a18c88ab9d
Camilo <ch385593@gmail.com>
2025-09-27 23:44:03 -0500

    cors mejorados

--- server.js
+++ server.js
@@ -13,11 +13,14 @@ const env=require('dotenv');
 env.config();
 
 //cors 
-app.use(cors({ 
-    origin:"*",
-    methods:["GET","POST","PUT","DELETE","PATCH"],
-    allowedHeaders:["Content-Type","Authorization"]
-}));
+const corsOptions = {
+    origin: 'http://localhost:5173',
+    methods: "GET,POST,PUT,DELETE,PATCH",
+    allowedHeaders: "Content-Type,Authorization",
+    optionsSuccessStatus: 204
+};
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
 //express.json
 app.use(express.json());
 
