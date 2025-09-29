5f12867657a995da4d5e44dfe3504b67362689d9
Camilo <ch385593@gmail.com>
2025-09-29 17:24:01 -0500

    cambios problematicos

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
-//archivos de ejecución
-const express=require('express');
-const cors = require('cors');
-const app=express();
-
-//swagger 
-const swaggerIU= require('swagger-ui-express')
-const specs=require('./swagger/swagger.js')
-
-//orden archivos de ejecución, cors, env, express.json, rutas, server 
-//dotenv
-const env=require('dotenv');
+//dotenv - debe ser lo primero para que las variables de entorno estén disponibles
+const env = require('dotenv');
 env.config();
+
+//archivos de ejecución
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+//swagger
+// const swaggerIU = require('swagger-ui-express')
+// const specs = require('./swagger/swagger.js')
+
+//express.json
+app.use(express.json());
 
 //cors 
 const corsOptions = {
@@ -21,40 +23,24 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 app.options('*', cors(corsOptions));
-//express.json
-app.use(express.json());
+
 
 //ruta
-    const personasRouter=require('./router/personasRouter');
-    const cursosRouter=require('./router/cursosRouter');
-    const docentesRouter=require('./router/docenteRouter');
-    const ofertaCursosRouter=require('./router/ofertaCursosRouter');
-    const docenteRouter=require('./router/docenteRouter');
-    const usuarioRouter=require('./router/usuarioRouter');
-    const pagoRouter=require('./router/pagoRouter');
-    const certificadoRouter=require('./router/certificadoRouter');
-    const asistenciaRouter=require('./router/asistenciaRouter');
-    const cursosMatriculados=require('./router/matricularCursosRouter')
-    app.use('/api/',pagoRouter);
-    app.use('/api/',certificadoRouter)
-    app.use('/api/', asistenciaRouter)
-    app.use('/api/',personasRouter);
-    app.use('/api/',cursosRouter);
-    app.use('/api/',docentesRouter);
-    app.use('/api/',ofertaCursosRouter);
-    app.use('/api/',docenteRouter);
-    app.use('/api/',cursosMatriculados)
-    app.use('/auth',usuarioRouter)
-    
+// const apiRouter = require('./router/index');
+// const usuarioRouter = require('./router/usuarioRouter');
 
- //swagger json
- app.use("/api-docs",swaggerIU.serve, swaggerIU.setup(specs) )
+// app.use('/api', apiRouter);
+// app.use('/auth', usuarioRouter)
+
+
+//swagger json
+// app.use("/api-docs", swaggerIU.serve, swaggerIU.setup(specs))
 
 
 
 
 //server 
-const PORT=process.env.PORT;
-app.listen(PORT,()=>{
-    console.log('Servidor en el puerto:',PORT);
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+    console.log('Servidor en el puerto:', PORT);
 })
